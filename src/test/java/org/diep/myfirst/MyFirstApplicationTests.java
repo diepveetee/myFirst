40492a3d1487d985a07b113bd1c17802f0464749
diepveetee <diepvt@mail.uc.edu>
2026-09-05 11:11:03 -0400

    Added MyFirstApplicationTests.java

---
+++ src/test/java/org/diep/myfirst/MyFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diep.myfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstApplicationTests {
+
+    public static void main(String[] args) {SpringApplication.run(MyFirstApplicationTests.class, args);
+    }
+
+}
